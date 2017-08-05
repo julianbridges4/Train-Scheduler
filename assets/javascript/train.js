8dd21d30987a12027ad54b04a1772974b9e7bffc
Julian K. Bridges <julianbridges01@gmail.com>
2017-08-04 21:30:58 -0400

    Created html structure, added styling, and initialized firebase

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBENIFRkNTZKzQs8u2D5_UQFDbF8IVAFgk",
+    authDomain: "train-scheduler-ed040.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ed040.firebaseio.com",
+    projectId: "train-scheduler-ed040",
+    storageBucket: "",
+    messagingSenderId: "515371926367"
+  };
+  firebase.initializeApp(config);
